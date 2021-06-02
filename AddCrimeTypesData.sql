e0d2a7ab4e2c890697ab9c17d0e16121cc73e64b
AllisonGibbons <agibbons67@gmail.com>
2021-06-02 03:52:17 -0700

    added crime types data

---
+++ AddCrimeTypesData.sql
@@ -0,0 +1,38 @@
+-- Script for adding data to CrimeTypes table
+
+INSERT INTO dbo.CrimeTypes 
+VALUES (
+	'Drug/Narcotic Offenses',
+	'Larceny-Theft',
+	'Robbery',
+	'Destruction/Damage/Vandalism of Property',
+	'Driving Under the Influence',
+	'Fraud Offenses',
+	'Kidnapping/Abduction',
+	'Counterfeiting/Forgery',
+	'Pornography/Obscene Material',
+	'Extortion/Blackmanil',
+	'Motor Vehicle Theft',
+	'Sex Offenses',
+	'Stolen Property Offenses',
+	'Embezzlement',
+	'Weapon Law Violations',
+	'Arson',
+	'Human Trafficking',
+	'Liquor Law Violations',
+	'Bad Checks',
+	'Prostitution Offenses',
+	'Family Offenses, Nonviolent',
+	'Curfew/Loitering/Vargrancy Violations',
+	'Animal Cruelty',
+	'Homicide Offenses',
+	'Peeping Tom',
+	'Sex Offenses, Consensual',
+	'Drunkenness',
+	'Bribery',
+	'Gambling Offenses',
+	'Trespass of Real Property',
+	'Assault Offenses',
+	'Burglary/Breaking&Entering'
+);
+Go
